bd970fe2aa776659dcea9e1e219565f5bc1dd793
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-02 14:19:50 -0400

    🚀

---
+++ App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="pb-2">gm Vite + React + Tailwind CSS!</p>
+        <p>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mb-2"
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,9 @@
+# Welcome to the Vite + React + Tailwind CSS Starter!
+
+## To install dependencies
+
+`npm install` or `yarn install`
+
+## To run the project locally
+
+`npm run dev` or `yarn dev`
