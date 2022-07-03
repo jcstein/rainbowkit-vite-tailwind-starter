318790ad09df409dda615e64e3fac8b3eaed76f3
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-03 00:01:02 -0400

    🌈

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,17 +9,11 @@ function App() {
     <div className="bg-zinc-900 flex justify-center items-center h-screen text-white">
       <div>
         <div className="flex justify-center text-2xl">
-          gm Vite + React + Tailwind CSS + RainbowKit!
+          gm ☀️ Vite + React + Tailwind CSS + RainbowKit!
         </div>
         <p className="mb-4 text-2xl"></p>
         <div className="flex justify-center">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded-full mb-4"
-            type="button"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
+          <ConnectButton />
         </div>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,92 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [
+    chain.mainnet,
+    chain.goerli,
+    chain.optimism,
+    chain.optimismKovan,
+    chain.polygon,
+    chain.polygonMumbai,
+    chain.arbitrum,
+  ],
+  [
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/eth",
+        };
+      },
+    }),
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/eth_goerli",
+        };
+      },
+    }),
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/optimism",
+        };
+      },
+    }),
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/optimism_testnet",
+        };
+      },
+    }),
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/polygon_mumbai",
+        };
+      },
+    }),
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: "https://rpc.ankr.com/arbitrum",
+        };
+      },
+    }),
+    publicProvider(),
+  ]
+);
+
+// you can also add a custom wallet list containing any of the following wallets: Argent, Brave Wallet, Coinbase Wallet, Injected Wallet, Ledger Live, MetaMask, Rainbow, Trust Wallet, Steakwallet, imToken, or WalletConnect. Learn more here: https://www.rainbowkit.com/docs/custom-wallet-list
+const { connectors } = getDefaultWallets({
+  appName: "Vite Tailwind RainbowKit Wagmi Starter",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} coolMode>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
