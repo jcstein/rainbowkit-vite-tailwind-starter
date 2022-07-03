cabcd012373d078bf53b8df36658c55c5eafc48e
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-03 01:30:12 -0400

    width screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
   return (
-    <div className="bg-zinc-900 flex justify-center items-center h-screen text-white">
+    <div className="bg-zinc-900 flex justify-center items-center h-screen text-white w-screen">
       <div>
         <div className="flex justify-center text-sm sm:text-base md:text-3xl lg:text-4xl pb-10">
           gm ☀️ Vite + React + Tailwind CSS + RainbowKit!
