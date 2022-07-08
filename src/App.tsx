42a1315d34c44813581b4b3419c821bf4b3680e7
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-08 02:28:33 -0400

    💅

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { BsGithub, BsTwitter } from "react-icons/bs";
 
 function App() {
   return (
@@ -9,6 +10,26 @@ function App() {
         </div>
         <div className="flex justify-center">
           <ConnectButton />
+        </div>
+        <div className="flex justify-center pt-10">
+          <a
+            href="https://github.com/jcstein/vite-tailwind-rainbowkit-starter"
+            rel="noopener nofollow noreferrer"
+            target="_blank"
+          >
+            <button className="flex items-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-xl text-sm px-5 py-2.5 text-center mr-2 mb-2">
+              <BsGithub />
+            </button>
+          </a>
+          <a
+            href="https://twitter.com/JoshCStein"
+            rel="noopener nofollow noreferrer"
+            target="_blank"
+          >
+            <button className="flex items-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-xl text-sm px-5 py-2.5 text-center mr-2 mb-2">
+              <BsTwitter />
+            </button>
+          </a>
         </div>
       </div>
     </div>
