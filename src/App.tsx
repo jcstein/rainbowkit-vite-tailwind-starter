496cc19347dfda6baa6a1ebb8d42fce11e2aef6c
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-14 17:17:55 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="bg-zinc-900 flex justify-center items-center h-screen text-white w-screen">
       <div>
-        <div className="flex justify-center text-sm sm:text-base md:text-3xl lg:text-4xl pb-10">
+        <div className="flex justify-center text-sm sm:text-base md:text-3xl lg:text-4xl pb-10 font-bold">
           gm 🌈 RainbowKit + Vite + React + Tailwind CSS!
         </div>
         <div className="flex justify-center">
